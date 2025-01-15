2e7df9e5509dcc440188c15d94d604065810358b
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2025-01-15 18:09:51 +0300

    güncel

--- Infrastructure/Persistence/Context/AppDbContext.cs
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace Persistence.Context
 {
-	public class AppDbContext
-	{
-		public AppDbContext()
-		{
-		}
+	public class AppDbContext : IdentityDbContext
+    {
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options) { }
 	}
 }
 
