75644dbc33aad05b195a524767fe3ff92c00f6d3
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2025-01-18 17:37:43 +0300

    18.01.25

--- Infrastructure/Persistence/DependencyInjection/ServiceContainer.cs
+++ Infrastructure/Persistence/DependencyInjection/ServiceContainer.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Persistence.Context;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence.DependencyInjection
 {
@@ -17,6 +21,40 @@ namespace Persistence.DependencyInjection
             services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(connectionString));
 
+
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                byte[] key = Encoding.ASCII.GetBytes("Qw12ER34TY56Ui78oi98v2bNh78JK4Hods7uUj12");
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                      
+                };
+            });
+
+
+
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+
+
+            }).AddEntityFrameworkStores<AppDbContext>();
             return services;
         }
     }
